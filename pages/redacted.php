8ef6b50a5a4fa34a14825949f8cca8adef8883a1
Vuk <104253978+Amizic@users.noreply.github.com>
2026-05-04 00:44:39 +0200

    New page
    Stay tuned!

---
+++ pages/redacted.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="/_null_sector/">
+    <link rel="stylesheet" href="css/regularTheme.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/utility.css">
+    <title>[REDACTED]</title>
+</head>
+
+<body>
+
+<?php require("../partials/header.php");?>
+
+<main>
+    <section class="column-section align-items-center text-align-center">
+
+        <h1 class="showoff-heading">[<span class="styled-font">RED</span>ACTED]</h1>
+        <h2>Stay tuned!</h2>    
+
+    </section>
+</main>
+
+<?php require("../partials/footer.php");?>
+<script src="js/time.js"></script>
+</body>
+</html>
