1a5b723eaa3304e189e9505e3a3677401873de48
Vuk <104253978+Amizic@users.noreply.github.com>
2026-05-04 00:47:39 +0200

    New navigation part
    More links!

--- partials/header.php
+++ partials/header.php
@@ -6,7 +6,7 @@
         <a class="nav-button" href="Home">HOME</a>
         <a class="nav-button" href="Services">SERVICES</a>
         <a class="nav-button" href="Team">TEAM</a>
-        <a class="nav-button styled-font" href="#">[REDACTED]</a>
+        <a class="nav-button styled-font" href="Redacted">[REDACTED]</a>
         <a class="nav-button" href="Partnership">PARTNERSHIP</a>
         <a class="nav-button" href="Contact">CONTACT</a>
     </nav>
